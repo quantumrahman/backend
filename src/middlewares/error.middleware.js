873db21abe36ae46af62dd1fcb25769b3ab50297
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-22 00:40:00 +0600

    feat: create error middleware and implement in app

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+
+import { ZodError } from 'zod';
+
+const errorMiddleware = (err, req, res, next) => {
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Internal server error';
+    let errors = [];
+
+    if (err instanceof ZodError) {
+        ((statusCode = 400), (message = 'Validation Failed'));
+
+        errors = err.issues.map((issue) => ({
+            field: issue.path.join('.'),
+            message: issue.message,
+        }));
+    }
+
+    return res.status(statusCode).json({
+        success: false,
+        message: message,
+        error: errors,
+    });
+};
+
+export default errorMiddleware;
